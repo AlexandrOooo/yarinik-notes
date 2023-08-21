8199ef7250638d98b77f46eb0c53ed47d97518f3
Olexander <grejnext@gmail.com>
2023-08-21 18:00:14 +0300

    feat: add delete and patch notes request

--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -31,7 +31,7 @@ export const getNote: RequestHandler = async (req, res, next) => {
 
 interface NoteBody {
   title?: string
-  text?: string
+  text: string
 }
 export const createNote: RequestHandler<
   unknown,
@@ -46,6 +46,62 @@ export const createNote: RequestHandler<
     }
     const newNote = await NoteModel.create({ title, text })
     res.status(201).json(newNote)
+  } catch (error) {
+    next(error)
+  }
+}
+interface NoteParams {
+  noteId: string
+}
+export const updateNote: RequestHandler<
+  NoteParams,
+  unknown,
+  NoteBody,
+  unknown
+> = async (req, res, next) => {
+  const noteId = req.params.noteId
+  const { title, text } = req.body
+  try {
+    if (!mongoose.isValidObjectId(noteId)) {
+      throw createHttpError(400, 'Invalid noteId')
+    }
+
+    if (!title) {
+      throw createHttpError(400, 'Title is required')
+    }
+
+    const note = await NoteModel.findById(noteId).exec()
+
+    if (!note) {
+      throw createHttpError(404, 'Note not found')
+    }
+    note.title = title
+    note.text = text
+
+    const updatedNote = await note.save()
+    res.status(200).json(updatedNote)
+  } catch (error) {
+    next(error)
+  }
+}
+export const deleteNote: RequestHandler<
+  NoteParams,
+  unknown,
+  NoteBody,
+  unknown
+> = async (req, res, next) => {
+  const noteId = req.params.noteId
+  try {
+    if (!mongoose.isValidObjectId(noteId)) {
+      throw createHttpError(400, 'Invalid noteId')
+    }
+
+    const note = await NoteModel.findByIdAndDelete(noteId).exec()
+
+    // if (!note) {
+    //   throw createHttpError(404, 'Note not found')
+    // }
+    res.sendStatus(204)
   } catch (error) {
     next(error)
   }
